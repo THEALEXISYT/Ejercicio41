0adddd5d19d771bdddb5abc48004fda93870bbef
Alexis <ajusgue2501@g.educaand.es>
2024-02-21 17:36:33 +0100

    Implementar método de suma en Calculadora

---
+++ CalculadoraEntornosGit/src/calculadoraentornosgit/Suma.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadoraentornosgit;
+
+/**
+ *
+ * @author Alexis
+ */
+    public class Suma {
+
+        public int sumar(int num1, int num2) {
+            return num1 + num2;
+        }
+
+    }
