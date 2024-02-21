0a0a8b00cdcfc72433cbb61c51b82c0d7588ebd3
Alexis <ajusgue2501@g.educaand.es>
2024-02-21 17:46:23 +0100

    Implementacion de dos metodos

---
+++ CalculadoraEntornosGit/src/calculadoraentornosgit/Division.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadoraentornosgit;
+
+/**
+ *
+ * @author Alexis
+ */
+public class Division {
+
+    public class dividir {
+
+        public int dividir(int num1, int num2) {
+            return num1 / num2;
+        }
+
+    }
+}
---
+++ CalculadoraEntornosGit/src/calculadoraentornosgit/Restar.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadoraentornosgit;
+
+/**
+ *
+ * @author Alexis
+ */
+public class Restar {
+        public class restar {
+
+        public int restar(int num1, int num2) {
+            return num1 - num2;
+        }
+
+    }
+}
